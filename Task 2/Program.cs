47b2d73210c73047ddca913ec16c3d8471b23327
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-14 00:38:09 +0300

    gone Task2

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Enter 1 number: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2 number: ");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+    Console.WriteLine(a);
+else if (a<b)
+    Console.WriteLine(b);    
