ae7f82d7dcd74bb9c028459e493c09ed5350fe8a
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-14 00:50:37 +0300

    gone Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа которая на вход принимает число и выдаёт является ли число четным ( делится ли оно на два без остатка)
+Console.Clear();
+Console.WriteLine("Enter number");
+int a = Convert.ToInt32(Console.ReadLine()!);
+if (a%2 == 0)
+{
+  Console.WriteLine("chetnoe");
+}
+else
+{
+  Console.WriteLine("nechetnoe");
+}
