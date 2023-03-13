deffeb3b1bcc1b523e2f63ca2340c2036601ac24
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-14 00:54:06 +0300

    gone Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа котое но вход принимает число N а на выходе показывает все чётные число от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine ()!);
+for(int i=1; i<n; i++) {
+  if(i%2==0)
+ Console.Write($"{i} ");
+}
