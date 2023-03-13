fbde074ae07befaa098a9ab39217dddfdf013435
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-14 00:45:22 +0300

    gone Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Enter 1 number: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2 number: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 3 number: ");
+int c=Convert.ToInt32(Console.ReadLine());
+if ((a>b) && (a>c)) 
+{
+    Console.WriteLine(a);
+}
+    else 
+if (b>c)
+    Console.WriteLine(b);
+    else
+    {
+      Console.WriteLine(c);
+    }
