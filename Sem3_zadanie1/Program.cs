6c9fb339463d4c58ea7c0e8a7c25677e7d79f209
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-18 12:42:59 +0300

    Готово

---
+++ Sem3_zadanie1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+string s =Console.ReadLine();
+if (s[0] == s[4] || s[1] == s[3]) {
+    Console.WriteLine($"{s} palindrome ");
+}
+else 
+    Console.WriteLine($"{s} no palindrome ");{   
+    }
