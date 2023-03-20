48aa95b432efc2464cdc66fb42d43e49eda1f695
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-20 11:43:27 +0300

    resit

---
+++ Sem3_new_zadanie1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter a five-digit number");
+string x = Console.ReadLine()!;
+char[] arr = x.ToCharArray();
+Array.Reverse(arr);
+string x2 = new String(arr);
+if (x == x2) Console.WriteLine("palindrome");
+else Console.WriteLine("no palindrome");
