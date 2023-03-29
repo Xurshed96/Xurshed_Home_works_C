2c73853647bb5cc22b744214e95b4fbcb7124b7a
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-29 18:16:26 +0300

    Разница

---
+++ Sem5_zadanie3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.03 7.85 22.13 2.05 78.48] -> 76.43
+
+Console.Clear();
+Console.WriteLine("Enter the size of the array");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[n];
+InputArrayRandomNumbers(numbers);
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($" Maximum value = {max}, minimum value = {min}");
+Console.WriteLine($"Difference between maximum and minimum value = {max - min}");
+
+void InputArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
