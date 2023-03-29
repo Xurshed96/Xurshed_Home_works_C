73e8b42503080327be05c280ee0773cf45ed91eb
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-03-29 18:05:35 +0300

    decided

---
+++ Sem5_zadanie1/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void InputArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " " );
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.Clear();
+Console.WriteLine("Enter the size of the array");
+int n = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[n];
+InputArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"{count} of them even");
