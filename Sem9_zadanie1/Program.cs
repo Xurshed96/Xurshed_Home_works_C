7b3c636d30d6a755d637b6f6b7b824e9961af354
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-04-08 00:14:47 +0300

    промежутке от N до 1

---
+++ Sem9_zadanie1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string f(int n)
+{
+  if (n == 1)
+    return "1";
+  return   $"{n} " + f(n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Enter the number: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
